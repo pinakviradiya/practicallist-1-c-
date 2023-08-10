eb3353be8925ae2ee665d68ad06479afc43abab1
pinakviradiya <139138055+pinakviradiya@users.noreply.github.com>
2023-08-10 17:51:51 +0530

    practical1iv.cpp

--- practical1iv.cpp
+++ practical1iv.cpp
@@ -4,19 +4,13 @@ using namespace std;
 int main() 
 {
     float r, d , c , a ;
-    // r = radius of the circle
-    // d = diameter of the circle
-    // c = circumference of the circle
-    // a = the area of the circle
-
+     cout<< "Viradiya pinak";
+     cout<< "220130318014";
     cout << "Enter the radius of the circle::";
     cin >> r;
-
-
     d = 2 * r;
     c = 2 * 3.14 * r;
     a = 3.14 * (r * r);
-
     cout << "\n";
     cout << "Diameter      :: = " << d << " units\n";
     cout << "Circumference :: = " << c << " units\n";
