b8151b8b10dc08a011c5e21d6c22cb799ff5454e
pinakviradiya <139138055+pinakviradiya@users.noreply.github.com>
2023-08-10 17:56:25 +0530

    practical1(3).cpp

--- practical1(3).cpp
+++ practical1(3).cpp
@@ -1,7 +1,9 @@
-//Write C++ program to display ìhello worldî
+//Write C++ program to display ‚Äúhello world‚Äù
 #include<iostream>
 using namespace std;
 int main()
-{
+{       
+	 cout<< "Viradiya pinak";
+         cout<< "220130318014";
 	cout<<"Hello world";
 }
